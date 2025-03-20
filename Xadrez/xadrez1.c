e3f765267776747490033a536e4e1891631b6e25
ToffeeJubileu <jocileudojcc@gmail.com>
2025-03-20 00:30:41 +0000

    concluido

---
+++ Xadrez/xadrez1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void moverTorre(int casas) {
+    if (casas > 0)
+    {
+        printf("Direita\n");
+        moverTorre(casas -1);
+    }
+    
+}
+
+void moverBispo(int casas) {
+    if (casas > 0)
+    {
+        printf("Cima, Direita\n");
+        moverBispo(casas -1);
+    }
+    
+}
+
+void moverRainha(int casas) {
+    if (casas > 0)
+    {
+        printf("Esquerda\n");
+        moverRainha(casas -1);
+    }
+    
+}
+
+void moverCavalo(int casas) {
+    if (casas > 0)
+    {
+        printf("cima\n");
+        moverCavalo(casas -1);
+    } else {
+        printf("Direita\n");
+    }
+    
+}
+
+int main() {
+    printf("##Torre: cinco casas para a direita.##\n");
+    moverTorre(5);
+    printf("##Bispo: cinco casas na diagonal para cima e à direita.##\n");
+    moverBispo(5);
+    printf("##Rainha: oito casas para a esquerda.##\n");
+    moverRainha(8);
+    printf("##Cavalo: uma vez em L para cima à direita.##\n");
+    moverCavalo(2);
+}
